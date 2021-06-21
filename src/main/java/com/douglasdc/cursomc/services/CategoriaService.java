5eeaa25bbbeb8a31bdbe26f5ed8d58e5e74dd9b6
Douglas Daniel Casagrande <douglasdcasagrande@hotmail.com>
2021-06-21 09:26:40 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/douglasdc/cursomc/services/CategoriaService.java
+++ src/main/java/com/douglasdc/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.douglasdc.cursomc.domain.Categoria;
 import com.douglasdc.cursomc.repositories.CategoriaRepository;
+import com.douglasdc.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,9 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
+		
+	
 }

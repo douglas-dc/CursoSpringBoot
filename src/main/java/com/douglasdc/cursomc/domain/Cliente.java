df6a1275420e064c02d5777204d4e2eac74c4f3e
Douglas Daniel Casagrande <douglasdcasagrande@hotmail.com>
2021-06-28 09:04:09 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/douglasdc/cursomc/domain/Cliente.java
+++ src/main/java/com/douglasdc/cursomc/domain/Cliente.java
@@ -50,7 +50,7 @@ public class Cliente implements Serializable {
 		this.nome = nome;
 		this.email = email;
 		this.cpfOuCnpj = cpfOuCnpj;
-		this.tipo = tipo.getCod();
+		this.tipo = (tipo == null) ? null : tipo.getCod(); //se o tipo for nulo, receberá valor nulo caso contrário receberá getCod
 	}
 
 	public Integer getId() {

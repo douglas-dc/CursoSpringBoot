b313afcc157141f50c759184b7fdc0ca9293a2f2
Douglas Daniel Casagrande <douglasdcasagrande@hotmail.com>
2021-06-29 09:32:29 -0300

    Validacao customizada de CPF ou CPNJ na insercao de cliente, corrigindo classe FieldMessage

--- src/main/java/com/douglasdc/cursomc/resources/exception/FieldMessage.java
+++ src/main/java/com/douglasdc/cursomc/resources/exception/FieldMessage.java
@@ -6,25 +6,25 @@ public class FieldMessage implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	private String fieldMessage;
+	private String fieldName;
 	private String message;
 	
 	public FieldMessage() {
 		
 	}
 
-	public FieldMessage(String fieldMessage, String message) {
+	public FieldMessage(String fieldName, String message) {
 		super();
-		this.fieldMessage = fieldMessage;
+		this.fieldName = fieldName;
 		this.message = message;
 	}
 
-	public String getFieldMessage() {
-		return fieldMessage;
+	public String getFieldName() {
+		return fieldName;
 	}
 
-	public void setFieldMessage(String fieldMessage) {
-		this.fieldMessage = fieldMessage;
+	public void setFieldMessage(String fieldName) {
+		this.fieldName= fieldName;
 	}
 
 	public String getMessage() {
@@ -34,6 +34,4 @@ public class FieldMessage implements Serializable {
 	public void setMessage(String message) {
 		this.message = message;
 	}
-	
-	
 }

d8fe55360124cf50a813ece6f7f3efec1d12fa49
Douglas Daniel Casagrande <douglasdcasagrande@hotmail.com>
2021-07-01 11:12:32 -0300

    Inserindo Pedido

--- src/main/java/com/douglasdc/cursomc/services/ProdutoService.java
+++ src/main/java/com/douglasdc/cursomc/services/ProdutoService.java
@@ -20,20 +20,21 @@ public class ProdutoService {
 
 	@Autowired
 	private ProdutoRepository repo;
-	
+
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	public Produto find(Integer id) {
 		Optional<Produto> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
 	}
-		
-	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
-		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy); 
+
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy,
+			String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		List<Categoria> categorias = categoriaRepository.findAllById(ids);
-		//return repo.search(nome, categorias, pageRequest);
+		// return repo.search(nome, categorias, pageRequest);
 		return repo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);
 	}
 }

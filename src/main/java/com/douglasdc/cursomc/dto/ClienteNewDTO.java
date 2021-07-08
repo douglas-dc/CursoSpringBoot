b47057c1729d804aa1aeef027802f23531f94227
Douglas Daniel Casagrande <douglasdcasagrande@hotmail.com>
2021-07-08 09:25:32 -0300

    Adicionando senha a Cliente

--- src/main/java/com/douglasdc/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/douglasdc/cursomc/dto/ClienteNewDTO.java
@@ -27,6 +27,9 @@ public class ClienteNewDTO implements Serializable {
 	private Integer tipo;
 	
 	@NotEmpty(message="Preenchimento obrigatório")
+	private String senha;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String logradouro;
 	
 	@NotEmpty(message="Preenchimento obrigatório")
@@ -82,6 +85,14 @@ public class ClienteNewDTO implements Serializable {
 
 	public void setTipo(Integer tipo) {
 		this.tipo = tipo;
+	}
+	
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 
 	public String getLogradouro() {
@@ -155,6 +166,4 @@ public class ClienteNewDTO implements Serializable {
 	public void setCidadeId(Integer cidadeId) {
 		this.cidadeId = cidadeId;
 	}
-	
-	
 }

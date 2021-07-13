63edb1bcedd283e4fb0eee7e69074e756ddf7b7b
Douglas Daniel Casagrande <douglasdcasagrande@hotmail.com>
2021-07-13 09:54:15 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/douglasdc/cursomc/CursomcApplication.java
+++ src/main/java/com/douglasdc/cursomc/CursomcApplication.java
@@ -1,12 +1,17 @@
 package com.douglasdc.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.douglasdc.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -14,7 +19,6 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-		
+		s3Service.uploadFile("C:\\Users\\douglas.casagrande\\Downloads\\imgTeste.png"); //por padrão usar 2 barras
 	}
 }

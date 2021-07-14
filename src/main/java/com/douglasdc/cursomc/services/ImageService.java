5a937015dcee176e5187fff1cad204bcebe2858a
Douglas Daniel Casagrande <douglasdcasagrande@hotmail.com>
2021-07-14 09:58:12 -0300

    Alterando versao da dependencia commons-io

--- src/main/java/com/douglasdc/cursomc/services/ImageService.java
+++ src/main/java/com/douglasdc/cursomc/services/ImageService.java
@@ -9,7 +9,7 @@ import java.io.InputStream;
 
 import javax.imageio.ImageIO;
 
-import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -19,7 +19,7 @@ import com.douglasdc.cursomc.services.exceptions.FileException;
 public class ImageService {
 
 	public BufferedImage getJpgImageFromFile(MultipartFile uploadedFile) {
-		String ext = FileUtils.getExtension(uploadedFile.getOriginalFilename());
+		String ext = FilenameUtils.getExtension(uploadedFile.getOriginalFilename());
 		if (!"png".equals(ext) && !"jpg".equals(ext)) {
 			throw new FileException("Somente imagens PNG e JPG são permitidas");
 		}
